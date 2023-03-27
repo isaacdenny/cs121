3e9d498f6d873bab62c9e521370924321c8c51d9
Isaac Denny <isaacquinndenny@gmail.com>
2023-03-27 12:46:11 -0600

    p3 - post constructor working

--- p3/Post.java
+++ p3/Post.java
@@ -1,9 +1,35 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.text.DecimalFormat;
 import java.time.Instant;
+import java.util.ArrayList;
 
 public class Post implements PostInterface {
+  private String author, text;
+  private Instant timestamp;
+  private int postID;
+  private ArrayList<String> comments;
 
   public Post(int id, String author, String text) {
-    // TODO constructor stub
+    DecimalFormat df = new DecimalFormat("00000.#");
+    this.postID = id;
+    this.author = author;
+    this.text = text;
+
+    this.timestamp = Instant.now();
+    this.comments = new ArrayList<String>();
+
+    try {
+      File postFile = new File("Post-" + df.format(postID) + ".txt");
+      if (postFile.createNewFile()) {
+        FileWriter fileWriter = new FileWriter(postFile);
+        String toWrite = df.format(postID) + " " + timestamp.toString() + " " + this.author + " " + this.text;
+        fileWriter.write(toWrite);
+        fileWriter.close();
+      }
+    } catch (Exception e) {
+      System.out.println(e.getMessage() != null ? e.getMessage() : "Error creating file " + postID);
+    }
   }
 
   public Post(int id) {
